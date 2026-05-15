a17a90152fb28f34698d07bde707835e75e6c406
emmagon-dev <emanuelalejandrogonzalezmolina@gmail.com>
2026-05-14 21:28:12 -0300

    Add files via upload

---
+++ ecommerce/Main.java
@@ -0,0 +1,14 @@
+
+
+
+import ui.MenuProducto;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MenuProducto menu = new MenuProducto();
+
+        menu.iniciar();
+    }
+}
